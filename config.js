2b845cb645c6c9b8ab33545df4074d953506a843
André Souza <84349114+andresouza-ap@users.noreply.github.com>
2026-08-20 10:21:19 -0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
   // Cole aqui a URL do seu Apps Script publicado como Web App
   // (Implantar > Nova implantação > Aplicativo da Web).
   // Deve terminar em /exec
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyNqXBvJMTcgyLWkFktQHdLy9w6T37pD-X4uLwz3ubOkEypOC19GWDA3YwAvoKWtjxH/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw10GMD-4D97j9BnyVojwp3NjMtTTBu2rFL9sfWBLUJyGP2NnY3EOjMC1XaSrYTPsJt/exec",
 
   // Janela de datas em que a sessão de Natal está disponível.
   SESSION_START: "2026-09-01",
