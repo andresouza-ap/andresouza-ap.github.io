d56dd50e1eafff8d0fcfcf9624748faaa1d4417f
André Souza <84349114+andresouza-ap@users.noreply.github.com>
2026-08-20 11:55:02 -0300

    Update config.js
    troca do periodo para novembro

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const CONFIG = {
   WEB_APP_URL: "https://script.google.com/macros/s/AKfycbz0DHvFzPjNGp8vmDrQN4vNzuxE7Fu0_zVgnyNWtF5bgB4aMb6oIJW-mNVyDqQFl6h9/exec",
 
   // Janela de datas em que a sessão de Natal está disponível.
-  SESSION_START: "2026-09-01",
-  SESSION_END:   "2026-09-30",
+  SESSION_START: "2026-11-01",
+  SESSION_END:   "2026-11-15",
 
   // Duração de cada sessão, em minutos (deve bater com o Code.gs).
   SLOT_MINUTES: 45,
